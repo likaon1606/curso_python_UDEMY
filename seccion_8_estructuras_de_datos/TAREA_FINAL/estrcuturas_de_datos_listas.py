3abb7eec20cd7d06799d3e444e145f7a170ae020
likaon1606 <ariel.fuentesgarcia@gmail.com>
2025-10-21 20:19:42 -0600

    tarea final seccion 8

---
+++ seccion_8_estructuras_de_datos/TAREA_FINAL/estrcuturas_de_datos_listas.py
@@ -0,0 +1,65 @@
+# *** Tarea 5 - Estructuras de datos: Listas ***
+
+# Ejercicio 1
+# Crea un programa que lea una secuencia de caracteres, guarde cada caracter en una posición de una lista y
+# finalmente muestre la secuencia invertida.
+
+
+
+
+
+# Ejercicio 2
+# Crea un programa que lea dos strings de la misma longitud, los guarde intercalados en una lista. Por último,
+# mostrar el contenido de la lista.
+# Por ejemplo, si introducimos hola caracola y adios marieta, debería mostrarse haodleao sc
+# amraarcioeltaa.
+
+
+
+
+
+# Ejercicio 3
+# Crea un programa que lea un string y guarde en una lista todas las consonantes.
+
+
+
+# Ejercicio 4
+# Crea un programa que lea una palabra, la guarde en una lista y compruebe si se trata de un palíndromo.
+
+
+# Ejercicio 5
+# Crea un programa que lea una matriz 3 x 3 y devuelva el máximo de cada fila.
+
+
+# Ejercicio 6
+# Crea un programa que lea un entero, n, de teclado y construya una matriz de tamaño n × n. Cada posición
+# debe contener su orden en la matriz (desde 0 hasta n2 − 1. Por ejemplo, si n es 3, deberá crearse la matriz
+# [0, 1, 2]
+# [3, 4, 5]
+# [6, 7, 8]
+
+
+
+# Ejercicio 7
+# Crea una matriz de n × m y asigna los valores manualmente. El programa debe indicar si la suma de cada
+# columna es el mismo valor.
+
+
+
+
+# Ejercicio 8
+# Crear un programa determina si la matriz introducida manualmente (tanto sus dimensiones como los elementos)
+# se trata de la matriz identidad. Recuerda que la matriz identidad debe ser una matriz cuadrada.
+
+
+
+
+# Ejercicio 9
+# Realiza un programa que calcule la matriz transpuesta.
+
+
+
+
+# Ejercicio 10
+# Crea un programa que pida al usuario la dimensión y cree la matriz identidad de orden correspondiente con
+# numpy.
